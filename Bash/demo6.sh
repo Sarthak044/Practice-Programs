640b8f3789a5386ad4a685e251263f5ce35e35ba
sarthak <sarthakkul044777@gmail.com>
2022-08-30 21:29:02 +0530

    latest

--- Bash/demo6.sh
+++ Bash/demo6.sh
@@ -4,6 +4,16 @@
 
 #Display the first three parameters. 
 
-echo "Parameter 1: ${1}"
-echo "Parameter 2: ${2}"
-echo "Parameter 3: ${3}"
+# echo "Parameter 1: ${1}"
+# echo "Parameter 2: ${2}"
+# echo "Parameter 3: ${3}"
+
+while [[ "${#}" -gt 0 ]]
+do 
+    echo "Number of Parameters: ${#}"
+    echo "Parameter 1: ${1}"
+    echo "Parameter 2: ${2}"
+    echo "Parameter 3: ${3}"
+    echo
+    shift
+done
