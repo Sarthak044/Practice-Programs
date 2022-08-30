c64e730d82e6e266792389f2b98237a7afe65946
sarthak <sarthakkul044777@gmail.com>
2022-08-30 13:35:22 +0530

    latest

--- Bash/demo5.sh
+++ Bash/demo5.sh
@@ -6,7 +6,7 @@ echo "${0}"
 
 echo "You used $(dirname ${0}) as the path to the $(basename ${0}) script."
 
-
+# We can see exit status via echo "${?}"
 # How many arguments they passed in.
 # (inside the script they are parameters, outside they are arguments.)
 
@@ -20,3 +20,18 @@ then
     echo "Usage: ${0} USERNAME [USERNAME] .. "
     exit 1
 fi
+
+#Generate and display a password for each parameter
+
+for USER_NAME in "${@}"
+do
+    PASSWORD=$(date +%s%N | sha256sum | head -c14)
+    echo "${USER_NAME}: ${PASSWORD}"
+done
+
+# Difference between ${*} and ${@}
+for USER_NAME in "${*}"
+do
+    PASSWORD=$(date +%s%N | sha256sum | head -c14)
+    echo "${USER_NAME}: ${PASSWORD}"
+done
