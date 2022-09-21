f5046af28439932aacbd4e69390b4c3af2ccfb7f
sarthak <sarthakkul044777@gmail.com>
2022-09-21 18:54:10 +0530

    latest

---
+++ python/crypto/Projects/project1/portfolio.py
@@ -0,0 +1,47 @@
+#!/usr/bin/python3
+
+#PORTFOLIO PROJECT
+
+#IMPORTS
+from unittest import result
+from colorama import Fore, Back, Style
+from prettytable import PrettyTable as pt
+import json
+import requests
+import os
+import csv
+
+
+#DECLARATION
+local_currency = 'INR'
+local_symbol = '₹'
+api_key = 'b14a28cc-f644-4cf5-b38c-37b2852f9f41'
+headers1 = {'X-CMC_PRO_API_KEY': api_key} # Default and same
+base_url = 'https://pro-api.coinmarketcap.com'
+
+# HEADER
+print()
+print("\t\t\tMY PORTFOLIO")
+print()
+
+# TABLE
+portfolio_value = 0.0
+
+table = pt(['Assest', 'Amount Owned', 'Value', 'Price', '1H', '24H', '7D'])
+
+with open("myportfolio.csv", "r") as csv_file:
+    csv_r = csv.reader(csv_file)
+    for line in csv_r:
+        if '\ufeff' in line[0]:
+            line[0] = line[0][1:].upper()
+        else:
+            line[0] = line[0].upper()
+        
+        symbol = line[0]
+        amount = line[1]
+        
+        quote_url = base_url + '/v1/cryptocurrency/quotes/latest?convert=' + local_currency + "&symbol=" + symbol
+        request = requests.get(quote_url, headers=headers1)
+        results = request.json()
+        #print(json.dumps(results, sort_keys=True, indent=4))
+        currency
